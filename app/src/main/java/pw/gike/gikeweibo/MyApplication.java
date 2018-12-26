a90daf19594f84ac98b6fc5d9d3a16e4bf8a7f25
Reborn <ren_xiaoyao@outlook.com>
2018-12-26 10:52:24 +0800

    1.Add retrofit lib 2.Fix build apk error(add proguard-rules)

--- app/src/main/java/pw/gike/gikeweibo/MyApplication.java
+++ app/src/main/java/pw/gike/gikeweibo/MyApplication.java
@@ -2,6 +2,7 @@ package pw.gike.gikeweibo;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.sina.weibo.sdk.WbSdk;
 import com.sina.weibo.sdk.auth.AuthInfo;
@@ -24,11 +25,17 @@ public class MyApplication extends Application {
         super.onCreate();
         context = getApplicationContext();
 
-        WbSdk.install(this,new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL,
+        WbSdk.install(this, new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL,
                 Constants.SCOPE));
     }
 
     public static Context getContext() {
         return context;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
